997a025632160204ebea673b71032b883c40c79c
Abel Tay <15604207+abeltay@users.noreply.github.com>
2022-12-04 13:53:17 +0800

    Add parsing helper to template

--- src/template_part1.kt
+++ src/template_part1.kt
@@ -1,6 +1,15 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        val inputLineRegex = """(\d+),(\d+) -> (\d+),(\d+)""".toRegex()
+
+        var answer = 0
+        for (it in input) {
+            val (startX, startY, endX, endY) = inputLineRegex
+                .matchEntire(it)
+                ?.destructured
+                ?: throw IllegalArgumentException("Incorrect input line $it")
+        }
+        return answer
     }
 
     val testInput = readInput("Day{replace}_test")
