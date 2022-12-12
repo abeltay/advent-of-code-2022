138aafcca598a24cbdf51f3f626027d50a03169d
Abel Tay <15604207+abeltay@users.noreply.github.com>
2022-12-12 12:49:43 +0800

    Parse input from file

--- src/Day11.kt
+++ src/Day11.kt
@@ -1,25 +1,68 @@
 fun main() {
+    val multiply = 0
+    val add = 1
     data class Data(
-        val items: List<List<Long>>,
-        val operations: List<(Long) -> Long>,
-        val passTo: List<(Long) -> Int>,
-        val divisor: Long
+        val items: ArrayDeque<Long>,
+        val operation: Int,
+        val operand: Long,
+        val divisor: Long,
+        val passTrue: Int,
+        val passFalse: Int
     )
 
-    fun part1(input: Data): Int {
-        val inspectionCount = MutableList(input.items.size) { 0 }
-        val items = mutableListOf<MutableList<Long>>()
-        for (it in input.items) {
-            items.add(it.toMutableList())
+    fun parseInput(input: List<String>): List<Data> {
+        var i = 0
+        val items = mutableListOf<Data>()
+        while (i < input.size) {
+            i++
+            val line2 = input[i].substringAfter(": ")
+            i++
+            val line3 = input[i].substringAfter(" = old ")
+            val operand = line3.substringAfter(' ')
+            i++
+            val divisor = input[i].substringAfter("Test: divisible by ").toLong()
+            i++
+            val passTrue = input[i].substringAfter("If true: throw to monkey ").toInt()
+            i++
+            val passFalse = input[i].substringAfter("If false: throw to monkey ").toInt()
+            i += 2
+            items.add(
+                Data(
+                    items = ArrayDeque(line2.split(", ").map { it.toLong() }),
+                    operation = if (line3[0] == '*') multiply else add,
+                    operand = if (operand != "old") operand.toLong() else 0,
+                    divisor = divisor,
+                    passTrue = passTrue,
+                    passFalse = passFalse,
+                )
+            )
         }
+        return items
+    }
+
+    fun part1(input: List<String>): Int {
+        val data = parseInput(input)
+        val inspectionCount = MutableList(data.size) { 0 }
         for (round in 0 until 20) {
-            for (i in items.indices) {
-                while (items[i].isNotEmpty()) {
-                    val first = items[i].removeFirst()
-                    val operated = input.operations[i](first) / 3
-                    inspectionCount[i]++
-                    val pass = input.passTo[i](operated)
-                    items[pass].add(operated)
+            for ((index, value) in data.withIndex()) {
+                while (value.items.isNotEmpty()) {
+                    val first = value.items.removeFirst()
+                    val calculated = if (value.operation == multiply) {
+                        var operand = value.operand
+                        if (operand == 0.toLong()) {
+                            operand = first
+                        }
+                        first * operand
+                    } else {
+                        first + value.operand
+                    } / 3
+                    inspectionCount[index]++
+                    val pass = if (calculated % value.divisor == 0.toLong()) {
+                        value.passTrue
+                    } else {
+                        value.passFalse
+                    }
+                    data[pass].items.add(calculated)
                 }
             }
         }
@@ -29,20 +72,33 @@ fun main() {
         return answer
     }
 
-    fun part2(input: Data): Long {
-        val inspectionCount = MutableList<Long>(input.items.size) { 0 }
-        val items = mutableListOf<MutableList<Long>>()
-        for (it in input.items) {
-            items.add(it.toMutableList())
+    fun part2(input: List<String>): Long {
+        val data = parseInput(input)
+        val inspectionCount = MutableList<Long>(data.size) { 0 }
+        var lcm = 1.toLong()
+        for (it in data) {
+            lcm *= it.divisor
         }
         for (round in 0 until 10000) {
-            for (i in input.items.indices) {
-                while (items[i].isNotEmpty()) {
-                    val first = items[i].removeFirst()
-                    val operated = input.operations[i](first) % input.divisor
-                    inspectionCount[i]++
-                    val pass = input.passTo[i](operated)
-                    items[pass].add(operated)
+            for ((index, value) in data.withIndex()) {
+                while (value.items.isNotEmpty()) {
+                    val first = value.items.removeFirst()
+                    val calculated = if (value.operation == multiply) {
+                        var operand = value.operand
+                        if (operand == 0.toLong()) {
+                            operand = first
+                        }
+                        first * operand
+                    } else {
+                        first + value.operand
+                    } % lcm
+                    inspectionCount[index]++
+                    val pass = if (calculated % value.divisor == 0.toLong()) {
+                        value.passTrue
+                    } else {
+                        value.passFalse
+                    }
+                    data[pass].items.add(calculated)
                 }
             }
         }
@@ -52,63 +108,11 @@ fun main() {
         return answer
     }
 
-    val testData = Data(
-        items = listOf(
-            listOf(79, 98),
-            listOf(54, 65, 75, 74),
-            listOf(79, 60, 97),
-            listOf(74)
-        ),
-        operations = listOf(
-            { old -> old * 19 },
-            { old -> old + 6 },
-            { old -> old * old },
-            { old -> old + 3 }
-        ),
-        passTo = listOf(
-            { num -> if (num % 23 == 0.toLong()) 2 else 3 },
-            { num -> if (num % 19 == 0.toLong()) 2 else 0 },
-            { num -> if (num % 13 == 0.toLong()) 1 else 3 },
-            { num -> if (num % 17 == 0.toLong()) 0 else 1 }
-        ),
-        divisor = 23 * 19 * 13 * 17
-    )
-    check(part1(testData) == 10605)
-    check(part2(testData) == 2713310158)
+    val testInput = readInput("Day11_test")
+    check(part1(testInput) == 10605)
+    check(part2(testInput) == 2713310158)
 
-    val actualData = Data(
-        items = listOf(
-            listOf(54, 61, 97, 63, 74),
-            listOf(61, 70, 97, 64, 99, 83, 52, 87),
-            listOf(60, 67, 80, 65),
-            listOf(61, 70, 76, 69, 82, 56),
-            listOf(79, 98),
-            listOf(72, 79, 55),
-            listOf(63),
-            listOf(72, 51, 93, 63, 80, 86, 81)
-        ),
-        operations = listOf(
-            { old -> old * 7 },
-            { old -> old + 8 },
-            { old -> old * 13 },
-            { old -> old + 7 },
-            { old -> old + 2 },
-            { old -> old + 1 },
-            { old -> old + 4 },
-            { old -> old * old }
-        ),
-        passTo = listOf(
-            { num -> if (num % 17 == 0.toLong()) 5 else 3 },
-            { num -> if (num % 2 == 0.toLong()) 7 else 6 },
-            { num -> if (num % 5 == 0.toLong()) 1 else 6 },
-            { num -> if (num % 3 == 0.toLong()) 5 else 2 },
-            { num -> if (num % 7 == 0.toLong()) 0 else 3 },
-            { num -> if (num % 13 == 0.toLong()) 2 else 1 },
-            { num -> if (num % 19 == 0.toLong()) 7 else 4 },
-            { num -> if (num % 11 == 0.toLong()) 0 else 4 }
-        ),
-        divisor = 17 * 2 * 5 * 3 * 7 * 13 * 19 * 11
-    )
-    println(part1(actualData))
-    println(part2(actualData))
+    val input = readInput("Day11")
+    println(part1(input))
+    println(part2(input))
 }
