3e1c9f998ece57430a0e27b63e5f8b0d7ed2ba0a
Abel Tay <15604207+abeltay@users.noreply.github.com>
2022-12-31 16:51:32 +0800

    Day 5: Supply Stacks

---
+++ src/Day05_part1.kt
@@ -0,0 +1,50 @@
+fun main() {
+    fun part1(input: List<String>): String {
+        fun convertInput(input: List<String>): MutableList<MutableList<Char>> {
+            val containers = mutableListOf<MutableList<Char>>()
+            for (i in 0 until input.last().length / 4 + 1) {
+                containers.add(mutableListOf())
+            }
+            for (i in input.indices.reversed()) {
+                if (i == input.size - 1) continue
+                for (j in containers.indices) {
+                    val container = 1 + j * 4
+                    if (container >= input[i].length) break
+                    val name = input[i][container]
+                    if (name != ' ') {
+                        containers[j].add(name)
+                    }
+                }
+            }
+            return containers
+        }
+
+        val emptyLineIndex = input.indexOf("")
+        val containers = convertInput(input.subList(0, emptyLineIndex))
+        val inputLineRegex = """move (\d+) from (\d+) to (\d+)""".toRegex()
+
+        for (it in input.subList(emptyLineIndex + 1, input.size)) {
+            val (s1, s2, s3) = inputLineRegex
+                .matchEntire(it)
+                ?.destructured
+                ?: throw IllegalArgumentException("Incorrect input line $it")
+            val number = s1.toInt()
+            val from = s2.toInt() - 1
+            val to = s3.toInt() - 1
+            val fromContainer = containers[from].takeLast(number)
+            containers[from] = containers[from].dropLast(number).toMutableList()
+            containers[to].addAll(fromContainer.reversed())
+        }
+        var answer = ""
+        for (it in containers) {
+            answer += it.last()
+        }
+        return answer
+    }
+
+    val testInput = readInput("Day05_test")
+    check(part1(testInput) == "CMZ")
+
+    val input = readInput("Day05")
+    println(part1(input))
+}
